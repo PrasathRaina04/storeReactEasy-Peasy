23541d1751c9d29428449c1282baf4c62c93951b
Prasath <prasathraina04@gmail.com>
2024-06-12 20:25:12 +0530

    Change is in CreateProduct Page

--- vite-project/src/Product/CreateProduct.page.jsx
+++ vite-project/src/Product/CreateProduct.page.jsx
@@ -7,14 +7,14 @@ export function CreateProduct(props) {
   const {setProductDetailes}= useStoreActions((actions)=> actions.productModel)
   const navigate = useNavigate();
 
-  const [product, setProduct] = useState({
-    productID: "",
+  const [product, setProduct] = useState([{
+    productID: productDetailes.length+1,
     productNameEng: "",
     productNameTn: "",
-    productKgs:"",
-    productQty:"",
+    productKgs:"250 g",
+    productQty:1,
     productPrice: "",
-  });
+  }]);
   // const [responseStatus, setResponseStatus] = useState("");
  
   function handleProductInput(inputType, e) {
@@ -25,20 +25,17 @@ export function CreateProduct(props) {
       case "productNameTn":
         setProduct({ ...product, productNameTn: e.target.value });
         break;
-      case "productDes":
-        setProduct({ ...product, productDescription: e.target.value });
+      case "productPrice":
+        setProduct({ ...product, productPrice: e.target.value });
         break;
-      case "productDesTn":
-        setProduct({ ...product, productDescriptionTn: e.target.value });
-        break;
-      case "cName":
-        setResponseStatus("")
-        setProduct({ ...product, cName: e.target.value });
-        break;
-      case "bName":
-        setResponseStatus("")
-        setProduct({ ...product, bName: e.target.value });
-        break;
+      // case "cName":
+      //   setResponseStatus("")
+      //   setProduct({ ...product, cName: e.target.value });
+      //   break;
+      // case "bName":
+      //   setResponseStatus("")
+      //   setProduct({ ...product, bName: e.target.value });
+      //   break;
     }
   }
   async function handleCreate() {
@@ -85,13 +82,10 @@ props.setShowCreateProduct(false)
           <input type="text" onChange={(e) => handleProductInput("productNameTn", e)} className="form-control" placeholder="Enter Product name in tamil" />
          </div>
         <div className="col-auto">
-          <label className="form-label float-start form-label form-label">Enter Product Description</label>
-          <input type="text" onChange={(e) => handleProductInput("productDes", e)} className="form-control" placeholder="Enter Product description " />
+          <label className="form-label float-start form-label form-label">Enter Product Price</label>
+          <input type="number" onChange={(e) => handleProductInput("productPrice", e)} className="form-control" placeholder="Enter Product description " />
          </div>
-        <div className="col-auto">
-          <label className="form-label float-start form-label form-label">Enter Product Description in Tamil</label>
-          <input type="text" onChange={(e) => handleProductInput("productDesTn", e)} className="form-control" placeholder="Enter Product Description in tamil" />
-         </div>
+       
       <div className="row d-flex justify-content-center" style={{ marginTop: "28px", }}>
         <div className="col-3" style={{ textAlign: "center", }}><button onClick={() => handleCreate()} className="btn btn-success" type="button" style={{ textAlign: "center", }}>Create</button></div>
         <div className="col-3" style={{ textAlign: "center", }}><button className="btn btn-danger" type="button" style={{ textAlign: "center", }} onClick={()=>props.setShowCreateProduct(false)}>cancel</button></div>
